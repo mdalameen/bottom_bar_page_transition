af71af130621df239f907aec8c949ec3e76c2b54
Mohamed Al Ameen <mdalameen.s@gmail.com>
2020-06-20 22:41:09 +0530

    initial version

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [0.0.1] - Initial Release.
+* Initial release.
+
+## [0.0.2] - Changed Description.
+* Changed description about the package.
---
+++ test/bottom_bar_page_transition_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// import 'package:bottom_bar_page_transition/bottom_bar_page_transition.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // final calculator = Calculator();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
